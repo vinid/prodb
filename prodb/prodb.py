442a6b947fa34ac4d35d8830433add33ccc462ae
vinid <chiccobia@gmail.com>
2020-12-10 20:46:21 +0100

    remove standardization

--- prodb/prodb.py
+++ prodb/prodb.py
@@ -297,9 +297,6 @@ class ProdB():
 
         return answers
 
-    def custom_standardization(self, input_data):
-        lowercase = tf.strings.lower(input_data)
-        return lowercase
 
     def get_vectorize_layer(self, texts, special_tokens=["[MASK]"]):
         """Build Text vectorization layer
@@ -316,7 +313,6 @@ class ProdB():
         vectorize_layer = TextVectorization(
             max_tokens=self.config.VOCAB_SIZE,
             output_mode="int",
-            standardize=self.custom_standardization,
             output_sequence_length=self.config.MAX_LEN,
         )
         vectorize_layer.adapt(texts)
